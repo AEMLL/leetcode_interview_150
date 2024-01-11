a1bd522711b171432db5cd778aeef45b0c78e246
AEMLL <80205461+AEMLL@users.noreply.github.com>
2024-01-11 15:22:24 +0000

    Solved h_index

---
+++ h_index.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        // merge sort
+        sort(citations.begin(), citations.end(), greater<int>());
+
+        int i;
+        for (i=0; (i < citations.size()) && (citations[i] >= i+1); i++);
+        return i;
+    }
+};
